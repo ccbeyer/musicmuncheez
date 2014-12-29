93f61ab02dbcb1c13d1819432edc6d11433c49d9
jackbeckwith <jackbeckwith@gmail.com>
2014-12-29 14:20:34 -0700

    seatgeek reformat

--- js/ticket_results.js
+++ js/ticket_results.js
@@ -3,19 +3,34 @@ $.ajax({
   success: function(data) {
     var events = [];
 
+    events.push(['<tr>', 
+      '<th>' + "Date" + '</th>',
+      '<th>' + "Event" + '</th>',
+      '<th>' + "Starting From" + '</th>',
+      '<th>' + "Buy!" + '</th>',
+    '</tr>'
+    ].join("\n"));
+
     // Make your html for the events table
     $.each(data.events, function (i, evt) {
+      var lowest_price = evt.stats.lowest_price;
+      if (typeof lowest_price == "object") {
+        lowest_price = "Sold out!";
+      } else {
+        lowest_price = "$" + lowest_price;
+      }
       events.push([
         '<tr>', 
           '<td>' + evt.datetime_local.split('T', 1).join("") + '</td>',
-          '<td><a href="' + evt.url + '">' + evt.title + '</a><br />' + evt.venue.name + '</td>',
+          '<td><a href="' + evt.url + '">' + evt.title + '</a><br />' 
+          + evt.venue.name + '<br />' + evt.venue.city + ', ' + evt.venue.state + '</td>',
+          '<td>' + lowest_price + '</td>',
           '<td><a href="' + evt.url + '" class="btn">Buy Tickets</a></td>',
         '</tr>'
       ].join("\n"));
     });
 
     //console.log(events);
-
     $(".events").append(events.join("\n"));
   },
   
