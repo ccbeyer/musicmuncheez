41b1b4803dc4bd0640091ca320f792b24dd5dd40
Chris Beyer <ccbeyer93aol.com>
2015-01-30 14:42:28 -0500

    fixed the merch pics n shit

--- public/js/etsy.js
+++ public/js/etsy.js
@@ -4,18 +4,28 @@ $.ajax({
   success: function(data) {
     console.log("yes");
     if (data.ok) {
-      $('#etsy-images').empty();
+      $('.merch').empty();
       if (data.count > 0) {
         $.each(data.results, function(i,item) {
-          $("<img/>").attr("src", item.Images[0].url_170x135).appendTo("#etsy-images").wrap(
-            "<a href='" + item.url + "'></a>");
+
+          $("<img/>").attr("src", item.Images[0].url_170x135).attr("class","merch-pic").appendTo(".merch").wrap(
+            "<div class='merch-pic-div'></div>");
         });
       } else {
-        ('<p>No results.</p>').appendTo('#etsy-images');
+        ('<p>No results.</p>').appendTo('.merch');
       }
     } else {
-      $('#etsy-images').empty();
+      $('.merch').empty();
       alert(data.error);
     }
   }
 });
+
+
+/*
+
+<div class="merch-pic-div">
+<img class="merch-pic" src="http://images.cdn.bigcartel.com/bigcartel/product_images/146357161/max_h-1000+max_w-1000/IMG_9966.jpeg"/>
+</div>
+
+*/
